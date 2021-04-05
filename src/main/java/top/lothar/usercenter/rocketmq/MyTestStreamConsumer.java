52bb9126d111565b9323b3df7e88c899bd7d8cb4
lutong <809676295@qq.com>
2021-04-05 15:25:02 +0800

    feat: stream流消费消息异常处理

--- src/main/java/top/lothar/usercenter/rocketmq/MyTestStreamConsumer.java
+++ src/main/java/top/lothar/usercenter/rocketmq/MyTestStreamConsumer.java
@@ -3,6 +3,8 @@ package top.lothar.usercenter.rocketmq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,6 +20,15 @@ public class MyTestStreamConsumer {
     @StreamListener(MySink.MY_INPUT)
     public void receive(String messageBody){
         log.info("自定义接口消费：通过stream收到了消息: {}", messageBody);
+        // 抛一场下边可以处理
+        // throw new IllegalArgumentException("XXX");
     }
+
+    @StreamListener("errorChannel")
+    public void error(Message<?> message) {
+        ErrorMessage errorMessage = (ErrorMessage) message;
+        System.out.println("Handling ERROR: " + errorMessage);
+    }
+
 
 }
